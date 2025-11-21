2aa08297582c4137eab0304c436096831ca75a68
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-21 12:53:07 +0530

    Added basic useState structure and conditional rendering based on empty array

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { Image, Pressable, Text, View } from "react-native";
+import { useState } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [notes, setNotes] = useState<
+    { id: number; title: string; content: string }[]
+  >([
+    {
+      id: 0,
+      title: "Note App",
+      content: "This is a simple app.",
+    },
+  ]);
+
   return (
     <View className="flex-1 bg-[#252525] px-7 py-14">
       <SafeAreaView className="flex-1">
@@ -19,16 +30,35 @@ export default function Index() {
           </Pressable>
         </View>
 
-        <View className="flex-1 items-center relative">
-          <Image
-            source={require("../assets/images/home-screen-empty.png")}
-            className="w-full"
-            resizeMode="contain"
-          ></Image>
-          <Text className="font-light text-xl text-white absolute top-125 sm:top-175 text-center">
-            Create your first note!
-          </Text>
-        </View>
+        {notes.length === 0 ? (
+          <View className="flex-1 items-center relative">
+            <Image
+              source={require("../assets/images/home-screen-empty.png")}
+              className="w-full"
+              resizeMode="contain"
+            ></Image>
+            <Text className="font-light text-xl text-white absolute top-125 sm:top-175 text-center">
+              Create your first note!
+            </Text>
+          </View>
+        ) : (
+          <View className="mt-8">
+            <FlatList
+              data={notes}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <View className="bg-[#FD99FF] w-full p-4 rounded-md">
+                  <Text className="text-[#111] text-3xl font-semibold">
+                    {item.title}
+                  </Text>
+                  <Text className="text-[#111] text-xl pt-1">
+                    {item.content}
+                  </Text>
+                </View>
+              )}
+            />
+          </View>
+        )}
       </SafeAreaView>
       <View className="bg-blue-500 justify-center items-center size-18 sm:size-20 elevation-2xl shadow-2xl p-2 rounded-full absolute bottom-[10%] right-[10%]">
         <FontAwesome6 name="plus" size={36} color="white" />
