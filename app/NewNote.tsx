8289b4d2817015ac4cfb1c979cdab2597a86f975
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-21 15:31:12 +0530

    Added functionality to NewNote screen and improved animations

--- app/NewNote.tsx
+++ app/NewNote.tsx
@@ -1,17 +1,24 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import React from "react";
-import { TextInput, View } from "react-native";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Pressable, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const NewNote = () => {
+  const [titleValue, setTitleValue] = useState("");
+  const [contentValue, setContentValue] = useState("");
+
   return (
     <View className="bg-[#252525] flex-1 py-12 px-8">
       <SafeAreaView className="flex-1">
         <View className="flex-row">
-          <View className="bg-[#3B3B3B] p-3.5 rounded-2xl mr-auto">
+          <Pressable
+            className="bg-[#3B3B3B] p-3.5 rounded-2xl mr-auto"
+            onPress={() => router.push("/")}
+          >
             <Ionicons name="chevron-back" size={24} color="white" />
-          </View>
+          </Pressable>
           <View className="bg-[#3B3B3B] p-3.5 rounded-2xl mr-6 sm:mr-8">
             <MaterialIcons name="visibility" size={24} color="white" />
           </View>
@@ -21,16 +28,21 @@ const NewNote = () => {
         </View>
 
         <View className="mt-10">
+          {/* TITLE */}
           <TextInput
             placeholder="Title"
             className="text-5xl text-white caret-blue-500 placeholder:text-[#9A9A9A]"
+            onChangeText={setTitleValue}
           ></TextInput>
+
+          {/* CONTENT */}
           <TextInput
             placeholder="Type something..."
             multiline
             numberOfLines={18}
             className="text-2xl text-white caret-blue-500 placeholder:text-[#9A9A9A] 
             mt-1 sm:mt-4 min-h-10"
+            onChangeText={setContentValue}
           ></TextInput>
         </View>
       </SafeAreaView>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,11 @@ import "../global.css";
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="NewNote" />
+      <Stack.Screen name="index" options={{ animation: "slide_from_left" }} />
+      <Stack.Screen
+        name="NewNote"
+        options={{ animation: "slide_from_right" }}
+      />
     </Stack>
   );
 }
